ca86c343aceca9968333106d2a5df370163ce4d3
ChloeC <yumeng0720@gmail.com>
2025-06-18 23:19:02 +1000

    add 1.fundamental python file

---
+++ 1.Fundamental.py
@@ -0,0 +1,25 @@
+print('Hello, World!')
+print(1+2)
+print()
+print("The end","or is it?","keep watching to learn more about Python",3)
+
+#strings 
+print('Today is a good day to learn Python.')
+print("Python is fun.")
+print("Python's string are easy to use")
+print("Hello" + " World")
+greeting ="hello"
+name = "Bruce"
+print(greeting + ' '+ name)
+
+#The Escape Character 
+splitstring = "This string has been\nsplitover\nseveral\nlines"
+print(splitstring)
+
+tabbedstring="1\t2\t3\t4\t5"
+print(tabbedstring)
+
+print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
+#or
+print("The pet shop owner said\"No, no, 'e's uh,...he's resting\".")
+print("""The pet shop owner said "No, no, 'e's uh,...he's resting".""")
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # PythonIntro2025
+data, string, list, dictionaries 
+programming daradigms, procedural programming, functional programming, object oriented programming 
+Python 3 
