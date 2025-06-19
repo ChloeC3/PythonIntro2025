72f5e9e4c01cd9646f129e21678e4bcaf18a38ff
ChloeC <yumeng0720@gmail.com>
2025-06-19 23:52:04 +1000

    escape character, variable

--- 1.Fundamental.py
+++ 1.Fundamental.py
@@ -12,7 +12,13 @@ greeting ="hello"
 name = "Bruce"
 print(greeting + ' '+ name)
 
+##
 #The Escape Character 
+#\n next line
+#\t tab to next stop 
+#\\ – literal backslash
+#\' – single quote
+#\" – double quote
 splitstring = "This string has been\nsplitover\nseveral\nlines"
 print(splitstring)
 
@@ -23,3 +29,44 @@ print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
 #or
 print("The pet shop owner said\"No, no, 'e's uh,...he's resting\".")
 print("""The pet shop owner said "No, no, 'e's uh,...he's resting".""")
+anotherSplitString = """This string has been \
+split over \
+several \
+lines"""
+print(anotherSplitString)
+
+print('Number\t 1 \t The Larch')
+
+#Each \\ is interpreted as a single backslash (\), because in strings, a single backslash is an escape character.
+print("C:\\users\\timbuchalka\\notes.txt") #recommended
+#Prefixing the string with r tells Python it's a raw string, meaning backslashes are treated literally — no escaping.
+print(r"C:\users\timbuchalka\notes.txt")
+
+##
+#Variables
+# 1. Begins with a letter / underscore _ character
+# 2. case sensitive 
+# 3. double quoation / single doesn't matter 
+age = 24
+print(age)
+print(type(greeting))
+print(type(age))
+
+age = '2 years'
+print(age)
+print(type(age))
+
+age_in_words = "2 years"
+print(name + " is " + age + " years old")
+print(type(age))
+
+##
+# Python Data Types 
+# 1. Numeric: int, float, complex 
+# 2. iterator 
+# 3. sequence (which are also iterators)
+# 4. mapping 
+# 5. file
+# 6. class 
+# 7. exception 
+
